78104dbd88bd890cfdd1b565e2276288b4b89d96
rindeal <dev.rindeal@gmail.com>
2024-09-04 20:29:57 +0200

    Create ref.ts

---
+++ src/ref.ts
@@ -0,0 +1,10 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Jan Chren ~rindeal
+ *
+ * SPDX-License-Identifier: GPL-3.0-only OR GPL-2.0-only
+ */
+
+export interface Ref {
+  name: string
+  hash: string
+}
