9b8fe760c6d11294d183271c3911fe471ac3f65b
rindeal <dev.rindeal@gmail.com>
2024-09-07 01:12:00 +0200

    Update GitCommandManager.ts

--- src/GitCommandManager.ts
+++ src/GitCommandManager.ts
@@ -4,72 +4,37 @@
  * SPDX-License-Identifier: GPL-3.0-only OR GPL-2.0-only
  */
 
-import { execFile } from 'child_process'
+import {
+  which,
+} from 'Utils'
 
 
-export class GitCommandManager {
-  gitVersion: string
+import {
+  GitVersionCommand,
+} from './GitCommands/Version'
+import {
+  GitLsRemoteCommand,
+} from './GitCommands/LsRemote'
+
+
+class GitCommandManager {
+  protected gitVersion: string = ''
+  protected gitPath: string = ''
 
   async initialize() {
-    const versionOutput = await this.execGit(['version'])
-    const versionMatch = versionOutput.match(/git version (\d+\.\d+\.\d+)/)
-    if ( ! versionMatch ) {
-      throw new Error('Unable to determine Git version')
-    }
-    this.gitVersion = versionMatch[1]
-  }
-
-  async execGit(args: string[]): Promise<string> {
-    return new Promise((resolve, reject) => {
-      execFile('git', args, (error, stdout) => {
-        if ( error ) {
-          reject(error)
-        } else {
-          resolve(stdout)
-        }
-      })
-    })
+    this.gitPath = await which('git')
+    const versionCmd = new GitVersionCommand(this.gitPath)
+    this.gitVersion = await versionCmd.execute()
   }
 
   async lsRemote(options: {
-    repository?: string,
+    remote?: string,
     branches?: boolean,
     tags?: boolean,
     exitCode?: boolean,
     patterns?: string[]
   }): Promise<string> {
-    const args = ['ls-remote']
-
-    if ( options.branches ) {
-      args.push('--heads')
-    }
-    if ( options.tags ) {
-      args.push('--tags')
-    }
-    if ( options.exitCode ) {
-      args.push('--exit-code')
-    }
-
-    args.push('--')
-
-    if ( options.repository ) {
-      if ( options.repository.startsWith('-') ) {
-        throw new Error('Invalid repository option')
-      }
-      args.push(options.repository)
-    }
-    if ( options.patterns ) {
-      if ( ! options.repository ) {
-        throw new Error('Option `repository` is needed for option `patterns`')
-      }
-      for (const pattern of options.patterns) {
-        if ( pattern.startsWith('-') ) {
-          throw new Error('Invalid pattern option')
-        }
-        args.push(pattern)
-      }
-    }
-
-    return this.execGit(args)
+    const lsRemoteCommand = new GitLsRemoteCommand(this.gitPath, options)
+    return lsRemoteCommand.execute()
   }
 }
