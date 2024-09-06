077074fddb29145d2fe366cf3e3198b4b1958b18
rindeal <dev.rindeal@gmail.com>
2024-09-07 00:53:49 +0200

    Create Utils.ts

---
+++ src/Utils.ts
@@ -0,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Jan Chren ~rindeal
+ *
+ * SPDX-License-Identifier: GPL-3.0-only OR GPL-2.0-only
+ */
+
+import * as path from 'path'
+import * as fs from 'fs/promises'
+
+
+export {
+  which,
+}
+
+
+async function which(executable: string): Promise<string | null> {
+  const dirs = process.env.PATH?.split(path.delimiter) || []
+  const checks = dirs.map(async (dir) => {
+    const fullPath = path.join(dir, executable)
+    try {
+      await fs.access(fullPath, fs.constants.X_OK)
+      return fullPath
+    } catch {
+      return null
+    }
+  })
+
+  const results = await Promise.all(checks)
+  return results.find(result => result) || null
+}
