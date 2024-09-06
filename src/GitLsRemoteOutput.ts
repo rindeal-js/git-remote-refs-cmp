275e0e8d76114f13437eb148e8caf7da0fc2aa58
rindeal <dev.rindeal@gmail.com>
2024-09-06 21:59:24 +0200

    Update and rename GitRemoteRepoRefMap.ts to GitLsRemoteOutput.ts

---
+++ src/GitLsRemoteOutput.ts
@@ -0,0 +1,29 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Jan Chren ~rindeal
+ *
+ * SPDX-License-Identifier: GPL-3.0-only OR GPL-2.0-only
+ */
+
+import {
+  GitRemoteRefMap,
+} from './GitRemoteRef'
+
+export {
+  GitLsRemoteOutput,
+}
+
+/**
+ * Represents the parsed output of the `git ls-remote` command.
+ */
+interface GitLsRemoteOutput {
+  /**
+   * The remote repository. This parameter can be either a URL or the name of a remote.
+   * Refer to the GIT URLS and REMOTES sections of git-fetch[1] for more details.
+   */
+  remote: string
+
+  /**
+   * A map of references in the remote repository.
+   */
+  refs: GitRemoteRefMap
+}
