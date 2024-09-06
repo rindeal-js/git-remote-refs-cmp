336c7dadd8e344d559c07fc7a0da11b6870e1910
rindeal <dev.rindeal@gmail.com>
2024-09-07 01:21:22 +0200

    Update GitRemoteRef.ts

--- src/GitRemoteRef.ts
+++ src/GitRemoteRef.ts
@@ -6,10 +6,10 @@
 
 export {
   GitRemoteRef,
-  GitRemoteRefMap,
   GitRemoteRefBase,
   SimpleGitRemoteRef,
 }
+
 
 /**
  * Represents a reference in a remote Git repository.
@@ -40,88 +40,11 @@ interface GitRemoteRef {
   oid: string
 }
 
-/**
- * A map of reference names to their corresponding object IDs.
- * 
- * @example
- * ```ts
- * const refMap: GitRemoteRefMap = new GitRemoteRefMap([
- *   {refname: 'refs/heads/master', oid: 'd1e8c3f4b5a6c7d8e9f0a1b2c3d4e5f6a7b8c9d0'},
- *   {refname: 'refs/tags/v1.0.1', oid: 'a1b2c3d4e5f6a7b8c9d0d1e8c3f4b5a6c7d8e9f0'},
- * ])
- * refMap.setRef({refname: 'refs/pull/1/head', oid: 'f6a7b8c9d0d1e8c3f4b5a6c7d8e9f0a1b2c3d4e5'})
- *
- * for (const ref of refMap.refs()) {
- *   console.log(ref.refname)
- *   console.log(ref.oid)
- * }
- * ```
- */
-class GitRemoteRefMap extends Map<string, string> {
-  constructor(refs?: GitRemoteRef[]) {
-    super(refs ? refs.map(ref => [ref.refname, ref.oid]) : [])
-  }
-
-  /**
-   * Returns an iterator of GitRemoteRef objects.
-   */
-  public *refs(): IterableIterator<GitRemoteRef> {
-    for (const [refname, oid] of this.entries()) {
-      yield { refname, oid }
-    }
-  }
-
-  /**
-   * Returns an iterator of reference names.
-   */
-  public *refnames(): IterableIterator<string> {
-    return this.keys()
-  }
-
-  /**
-   * Returns an iterator of object IDs.
-   */
-  public *oids(): IterableIterator<string> {
-    return this.values()
-  }
-
-  /**
-   * Sets a reference in the map.
-   * 
-   * @param ref - The GitRemoteRef to set.
-   * @returns The updated GitRemoteRefMap.
-   */
-  public setRef(ref: GitRemoteRef): this {
-    this.set(ref.refname, ref.oid)
-    return this
-  }
-
-  /**
-   * Gets a reference by its name.
-   * 
-   * @param refname - The name of the reference.
-   * @returns The GitRemoteRef or undefined if not found.
-   */
-  public getRef(refname: string): GitRemoteRef | undefined {
-    const oid = this.get(refname)
-    return oid ? { refname, oid } : undefined
-  }
-
-  /**
-   * Executes a callback for each reference in the map.
-   * 
-   * @param callback - The callback to execute.
-   */
-  public forEachRef(callback: (ref: GitRemoteRef) => void): void {
-    for (const ref of this.refs()) {
-      callback(ref)
-    }
-  }
-}
-
-class GitRemoteRefBase implements GitRemoteRef {
-  static _gitOidRegExp = /^[0-9a-f]{40}$/
-  static _gitRefNameRegExp = /^(?:refs\/(?:heads|tags|remotes)\/|HEAD$)/
+abstract class GitRemoteRefBase implements GitRemoteRef {
+  public abstract refname: string
+  public abstract oid: string
+  protected static _gitOidRegExp = /^[0-9a-f]{40}$/
+  protected static _gitRefNameRegExp = /^(?:refs\/(?:heads|tags|remotes)\/|HEAD$)/
 
   public static validateOid(oid: string): boolean {
     return GitRemoteRefBase._gitOidRegExp.test(oid)
@@ -146,9 +69,6 @@ class GitRemoteRefBase implements GitRemoteRef {
  * ```
  */
 class SimpleGitRemoteRef extends GitRemoteRefBase {
-  refname: string;
-  oid: string;
-
   constructor({ refname, oid }: GitRemoteRef) {
     if ( ! ( oid && GitRemoteRefBase.validateOid(oid) ) ) {
       throw new Error(`Invalid OID: \`${oid}\``)
