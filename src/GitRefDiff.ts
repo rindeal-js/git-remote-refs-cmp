3b0c48f83336846b46d02a8094f83269fa6f31b7
rindeal <dev.rindeal@gmail.com>
2024-09-07 03:15:12 +0200

    Update GitRefDiff.ts

--- src/GitRefDiff.ts
+++ src/GitRefDiff.ts
@@ -8,21 +8,14 @@ import {
   GitRemoteRef,
   GitRemoteRefMap,
 } from './GitRemoteRef'
+import {
+  GitRefDiffType,
+} from './GitRefDiffType'
 
 
 export {
-  GitRefDiffType,
   GitRefDiff,
   GitRefDiffBase,
-}
-
-
-enum GitRefDiffType {
-  UNKNOWN = 'UNKNOWN',
-  ZERO_REFS = 'ZERO_REFS',
-  REF_COUNT_MISMATCH = 'REF_COUNT_MISMATCH',
-  REF_NOT_FOUND = 'REF_NOT_FOUND',
-  OID_MISMATCH = 'OID_MISMATCH',
 }
 
 
