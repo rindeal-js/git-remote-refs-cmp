9bf2c12da63f94abe04fdd1b0554d3bcff044b7c
rindeal <dev.rindeal@gmail.com>
2024-09-07 03:09:01 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,26 @@
  * SPDX-License-Identifier: GPL-3.0-only OR GPL-2.0-only
  */
 
-import { Repo , RefDiff } from './Repo'
+import {
+  GitLsRemoteRefDiff,
+} from './GitLsRemoteRefDiff'
+import {
+  GitRemoteRefsCmp,
+} from './GitRemoteRefsCmp'
 
 
-export { Logger } from './Logger'
-export { Repo , RefDiff , ZeroRefs , RefCountMismatch , RefNotFound , HashMismatch } from './Repo'
+export type { GitLsRemoteRefDiff } from './GitLsRemoteRefDiff'
+export type { GitRemoteRefsCmp } from './GitRemoteRefsCmp'
+export type { Logger } from './Logger'
 
 
-export async function refsDiffer(sourceUrl: string, targetUrl: string): Promise<RefDiff | null> {
-  const sourceRepo = new Repo(sourceUrl)
-  const targetRepo = new Repo(targetUrl)
-  return await sourceRepo.refsDiffer(targetRepo)
+export {
+  gitRemoteRefsCmp,
+}
+
+
+async function gitRemoteRefsCmp(sourceRemote: string, targetRemote: string): Promise<GitLsRemoteRefDiff | null> {
+  const cmp = new GitRemoteRefsCmp()
+  await cmp.init()
+  return cmp.compare(sourceRemote, targetRemote)
 }
